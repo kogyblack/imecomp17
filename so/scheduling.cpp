f0095a39e6737f201a9bf47472651a6ddf2428b1
Naum Azeredo <naumazeredo@gmail.com>
2016-04-28 13:52:28 -0300

    Updated program

--- so/scheduling.cpp
+++ so/scheduling.cpp
@@ -58,31 +58,11 @@ int main() {
     q1.push_back(process);
   }
 
-#ifdef DEBUG
-  printf("\n\n");
-  printf("Time      : tempo em ms\n");
-  printf("Queue     : fila da qual o processo é escalonado (none se nenhum processo foi escalonado)\n");
-  printf("pid       : identificador do processo escalonado (none se nenhum processo foi escalonado)\n");
-  printf("Q1 timeout: pid dos processos que receberam timeout na fila Q1\n");
-  printf("IO queue  : pid do processo utilizado a I/O\n");
-  printf("\n\n");
-
-  printf("Time| Queue/pid       |Q1 timeout|IO queue\n");
-#endif
-
   while (!q0.empty() || !q1.empty() || !io.empty()) {
-#ifdef DEBUG
-    printf("%3d", timer);
-#endif
-
     if (!q0.empty()) { // Se a fila q0 não for vazia, escalona o processo dessa fila
       Process& p = q0.front();
       p.burst++;
       p.timeout++;
-
-#ifdef DEBUG
-      printf(" | Q0 %d", p.pid);
-#endif
 
       // Adiciona no diagrama de Gantt
       if (gantt_pid.empty() || gantt_pid.back() != p.pid) {
@@ -91,45 +71,24 @@ int main() {
       }
 
       if (p.burst == p.total_burst) { // Se o processo acabou o burst
-#ifdef DEBUG
-        printf(" (burst");
-#endif
         if (p.io > 0) { // Envia o processo para a fila de I/O se ainda tiver que executar alguma I/O
-#ifdef DEBUG
-          printf("|io)");
-#endif
           p.io--;
           p.timeout = 0;
           p.io_start_time = timer + 1;
           io.push(p);
-#ifdef DEBUG
-        } else {
-          printf(")   ");
-#endif
         }
         q0.pop();
         // Se não, o processo finalizou
       } else if (p.timeout == rr_timeout) { // Se o processo completou o tempo do Round-Robin, envia o processo para a fila q1
-#ifdef DEBUG
-        printf(" (rr)      ");
-#endif
         p.timeout = -1;
         q1.push_back(p);
         q0.pop();
-#ifdef DEBUG
-      } else {
-        printf("           ");
-#endif
       }
     } else if (!q1.empty()) { // Se a fila q0 for vazia mas a fila q1 não for, escalona o processo dessa fila
       Process& p = q1.front();
       p.burst++;
       p.timeout = -1;
 
-#ifdef DEBUG
-      printf(" | Q1 %d", p.pid);
-#endif
-
       // Adiciona no diagrama de Gantt
       if (gantt_pid.empty() || gantt_pid.back() != p.pid) {
         gantt_pid.push_back(p.pid);
@@ -137,34 +96,16 @@ int main() {
       }
 
       if (p.burst == p.total_burst) { // Se o processo acabou o burst
-#ifdef DEBUG
-        printf(" (burst");
-#endif
         if (p.io > 0) { // Envia o processo para a fila de I/O se ainda tiver que executar alguma I/O
-#ifdef DEBUG
-          printf("|io)");
-#endif
           p.io--;
           p.timeout = 0;
           p.io_start_time = timer + 1;
           io.push(p);
-#ifdef DEBUG
-        } else {
-          printf(")   ");
-#endif
         }
         q1.erase(q1.begin());
         // Se não, o processo finalizou
-#ifdef DEBUG
-      } else {
-        printf("           ");
-#endif
       }
     } else {
-#ifdef DEBUG
-      printf(" | (none)         ");
-#endif
-
       // Adiciona no diagrama de Gantt
       if (gantt_pid.empty() || gantt_pid.back() != -1) {
         gantt_pid.push_back(-1);
@@ -173,15 +114,9 @@ int main() {
     }
 
     // Atualiza os timeouts da fila q1
-#ifdef DEBUG
-    printf(" |");
-#endif
     for (int i = 0; i < q1.size(); ++i) {
       q1[i].timeout++;
       if (q1[i].timeout == q1_timeout) {
-#ifdef DEBUG
-        printf(" %d", q1[i].pid);
-#endif
         q0.push(q1[i]);
         q0.back().timeout = 0;
       }
@@ -194,26 +129,13 @@ int main() {
     if (!io.empty() && io.front().io_start_time <= timer) {
       Process& p = io.front();
       p.timeout++;
-#ifdef DEBUG
-      printf(" | %d", p.pid);
-#endif
       if (p.timeout == io_timeout) {
-#ifdef DEBUG
-        printf(" (timeout)");
-#endif
         p.burst = 0;
         p.timeout = 0;
         q0.push(p);
         io.pop();
       }
-#ifdef DEBUG
-    } else {
-      printf(" | ");
-#endif
     }
-#ifdef DEBUG
-    printf("\n");
-#endif
 
     timer++;
   }
@@ -225,7 +147,7 @@ int main() {
     if (gantt_pid[i] >= 0)
       printf("%2d\n", gantt_pid[i]);
     else
-      printf(" (none)\n");
+      printf(" (nenhum)\n");
   }
-  printf("%4d\t (finished)\n", timer);
+  printf("%4d\t (finalizado)\n", timer);
 }
